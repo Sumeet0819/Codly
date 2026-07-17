4a735e5cf03593ae7971e123dbedd5ff06c28100
Sumeet T <>
2026-07-17 10:31:18 +0530

    chore: remove obsolete API testing and compiler fetching scripts

--- fetch_compilers.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const test = async () => {
-  const response = await fetch("https://wandbox.org/api/list.json");
-  const data = await response.json();
-  const compilers = data.map(d => ({ lang: d.language, name: d.name, compiler: d.compiler }));
-  fs.writeFileSync('compilers.json', JSON.stringify(compilers, null, 2));
-};
-test();
--- test_wandbox.js
+++
@@ -1,8 +0,0 @@
-const test = async () => {
-  const response = await fetch("https://wandbox.org/api/list.json");
-  const data = await response.json();
-  const langs = new Set();
-  data.forEach(d => langs.add(d.language));
-  console.log([...langs]);
-};
-test();
